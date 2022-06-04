858108be047bd7551cafd8a48e4b00708c9304fd
javohir101dev <uralovjavohir25@gmail.com>
2022-06-04 17:02:56 +0500

    command line runner

--- src/main/java/com/example/userservice/UserserviceApplication.java
+++ src/main/java/com/example/userservice/UserserviceApplication.java
@@ -3,6 +3,7 @@ package com.example.userservice;
 import com.example.userservice.domain.Role;
 import com.example.userservice.domain.User;
 import com.example.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,39 +15,45 @@ import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
+    @Value("${spring.sql.init.mode}")
+    String runModeType;
 
     public static void main(String[] args) {
         SpringApplication.run(UserserviceApplication.class, args);
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    CommandLineRunner runner(UserService userService){
-//        return args -> {
-//            userService.saveRole(new Role(null, "ROLE_USER"));
-//            userService.saveRole(new Role(null, "ROLE_MANAGER"));
-//            userService.saveRole(new Role(null, "ROLE_ADMIN"));
-//            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-//
-//            userService.saveUser(new User(null, "Javohir Uralov", "java", "123", new ArrayList<>()));
-//            userService.saveUser(new User(null, "Will Smith", "will", "123", new ArrayList<>()));
-//            userService.saveUser(new User(null, "Jim Carry", "jim", "123", new ArrayList<>()));
-//            userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "123", new ArrayList<>()));
-//
-//            userService.addRoleToUser("java", "ROLE_USER");
-//            userService.addRoleToUser("java", "ROLE_MANAGER");
-//            userService.addRoleToUser("will", "ROLE_MANAGER");
-//            userService.addRoleToUser("jim", "ROLE_ADMIN");
-//            userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
-//            userService.addRoleToUser("arnold", "ROLE_ADMIN");
-//            userService.addRoleToUser("arnold", "ROLE_USER");
-//
-//        };
-//
-//    }
+    @Bean
+    CommandLineRunner runner(UserService userService) {
+
+        if (runModeType.equals("always")) {
+            return args -> {
+                userService.saveRole(new Role(null, "ROLE_USER"));
+                userService.saveRole(new Role(null, "ROLE_MANAGER"));
+                userService.saveRole(new Role(null, "ROLE_ADMIN"));
+                userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+                userService.saveUser(new User(null, "Javohir Uralov", "java", "123", new ArrayList<>()));
+                userService.saveUser(new User(null, "Will Smith", "will", "123", new ArrayList<>()));
+                userService.saveUser(new User(null, "Jim Carry", "jim", "123", new ArrayList<>()));
+                userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "123", new ArrayList<>()));
+
+                userService.addRoleToUser("java", "ROLE_USER");
+                userService.addRoleToUser("java", "ROLE_MANAGER");
+                userService.addRoleToUser("will", "ROLE_MANAGER");
+                userService.addRoleToUser("jim", "ROLE_ADMIN");
+                userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+                userService.addRoleToUser("arnold", "ROLE_ADMIN");
+                userService.addRoleToUser("arnold", "ROLE_USER");
+
+            };
+        }
+        return null;
+
+    }
 
 }
